4a115b40116e735fae9e0c3572001429b41436f7
Daniele Sassoli <danielesassoli@gmail.com>
2025-02-18 18:34:49 -0800

    Add Post Review Comment permission and seed defaults
    Introduce the postReviewComment permission for posting review comments,
    add the schema helper to grant it on refs/* for Registered Users, and
    include it in default All-Projects ACLs. Update tests to confirm the new
    permission is setup correctly.
    
    For an online upgrade, this schema migration should be run by all hosts
    before enabling the new permission. The permission needs to be added to
    allow users without label voting permission to continue posting comments
    on changes.
    
    Forward-Compatible: checked
    Release-Notes: Added schema migration for the new Post Review Comment permission
    Change-Id: I81c864c7631c5e64105fc165fd7b2d9d5b5540a5

--- java/com/google/gerrit/entities/Permission.java
+++ java/com/google/gerrit/entities/Permission.java
@@ -60,6 +60,7 @@ public abstract class Permission implements Comparable<Permission> {
   public static final String SUBMIT = "submit";
   public static final String SUBMIT_AS = "submitAs";
   public static final String TOGGLE_WORK_IN_PROGRESS_STATE = "toggleWipState";
+  public static final String POST_REVIEW_COMMENT = "postReviewComment";
   public static final String VIEW_PRIVATE_CHANGES = "viewPrivateChanges";
   public static final String AI_REVIEW = "aiReview";
 
@@ -99,6 +100,7 @@ public abstract class Permission implements Comparable<Permission> {
     NAMES_LC.add(SUBMIT.toLowerCase(Locale.US));
     NAMES_LC.add(SUBMIT_AS.toLowerCase(Locale.US));
     NAMES_LC.add(TOGGLE_WORK_IN_PROGRESS_STATE.toLowerCase(Locale.US));
+    NAMES_LC.add(POST_REVIEW_COMMENT.toLowerCase(Locale.US));
     NAMES_LC.add(VIEW_PRIVATE_CHANGES.toLowerCase(Locale.US));
     NAMES_LC.add(AI_REVIEW.toLowerCase(Locale.US));
 
